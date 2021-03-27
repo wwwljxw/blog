87a36e8855ac7cdc80b1563b037545f8327abf3e
Linjx <ljx282018@icloud.com>
2021-03-27 20:57:55 +0800

    添加实体类

---
+++ src/main/java/com/ljx/blog/dao/UserDao.java
@@ -0,0 +1,20 @@
+package com.ljx.blog.dao;
+
+
+import com.ljx.blog.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Description: 用户持久层接口
+ * @Author: ONESTAR
+ * @Date: Created in 11:11 2020/3/26
+ * @QQ群: 530311074
+ * @URL: https://onestar.newstar.net.cn/
+ */
+@Mapper
+@Repository
+public interface UserDao {
+    User findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+}
