cf7d11ee26a0f279fe8354a3364a38e6abc0152e
Linjx <ljx282018@icloud.com>
2021-03-27 20:45:56 +0800

    重构项目

--- src/main/java/com/ljx/blog/service/LoginService.java
+++
@@ -1,7 +0,0 @@
-package com.ljx.blog.service;
-
-import com.ljx.blog.pojo.TUser;
-
-public interface LoginService {
-    TUser getNaPwd(String username);
-}
--- src/test/java/com/ljx/blog/BlogApplicationTests.java
+++ src/test/java/com/ljx/blog/BlogApplicationTests.java
@@ -1,14 +1,10 @@
 package com.ljx.blog;
 
-import com.github.pagehelper.PageInfo;
-import com.ljx.blog.Mapper.TBlogMapper;
-import com.ljx.blog.pojo.TBlog;
-import com.ljx.blog.pojo.TType;
-import com.ljx.blog.service.BlogsService;
-import com.ljx.blog.service.TypeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 import java.util.List;
 
@@ -16,15 +12,10 @@ import java.util.List;
 class BlogApplicationTests {
 
     @Autowired
-    private TBlogMapper tBlogMapper;
 
     @Test
     void contextLoads() {
-        List<TBlog> allBlog = tBlogMapper.getAllBlog();
-        for (int i = 0; i < allBlog.size(); i++) {
-            TBlog tBlog = allBlog.get(i);
-            System.out.println(tBlog.getType());
-        }
+
     }
 
 }
